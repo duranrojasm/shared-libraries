df2f18006c9f302ff1509b78e5a75330707693d6
Valentina <duranrojasm@gmail.com>
2021-10-28 11:43:20 -0500

    sin echo

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -2,7 +2,6 @@
 
 def downloadSources(String branch, String url, String credential) {
     public static final String GIT_COMMIT
-    root.echo branch+" "+url+" "+credential
     checkout([
         $class: "GitSCM",
         branches: [[name: "*/${branch}"]],
