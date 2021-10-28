e8ff830d718bf6e8d3f03ce977325e3d80fc5b40
Valentina <duranrojasm@gmail.com>
2021-10-28 14:46:11 -0500

    tool sonar

--- src/com/devsecops/steps/sonar/SonarScanner.groovy
+++ src/com/devsecops/steps/sonar/SonarScanner.groovy
@@ -8,7 +8,7 @@ class SonarScanner extends StepAbstract {
 
     void run() {
         root.dir(this.source) {
-            def scannerHome = tool 'sonarQubeScanner46'
+            def scannerHome = tool name: 'sonarQubeScanner46'
             root.withSonarQubeEnv ('sonarqube') {
                 root.sh "${this.command}"
             }
